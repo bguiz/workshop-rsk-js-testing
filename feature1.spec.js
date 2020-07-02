38fde455ef511619ef0cabca96fd7eece334ddab
bguiz <bguiz@users.noreply.github.com>
2020-07-02 17:46:26 +0800

    docs: JS testing with mocha workshop adapted for RSK
    - original from https://dappsdev.org/hands-on/mocha-intro/

---
+++ feature1.spec.js
@@ -0,0 +1,15 @@
+// tests for: feature1
+
+const assert = require('assert');
+
+const { add } = require('./feature1.js');
+
+describe('feature1', () => {
+
+  describe('add', () => {
+
+    // tests go here
+
+  });
+
+});
